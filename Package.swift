c57cd4ead3d702ea43d6c897be39c5fa21d2c92d
Amy While <26681721+CharlieWhile13@users.noreply.github.com>
2021-03-31 19:33:34 +0100

    SPM Support?

--- Package.swift
+++ Package.swift
@@ -20,9 +20,10 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "LNZTreeView",
-            dependencies: []),
-        .testTarget(
-            name: "LNZTreeViewTests",
-            dependencies: ["LNZTreeView"]),
+            dependencies: [],
+            path: "LNZTreeView",
+            cSettings: [
+                .headerSearchPath("Private")
+            ]),
     ]
 )
--- Sources/LNZTreeView/LNZTreeView.swift
+++
@@ -1,3 +0,0 @@
-struct LNZTreeView {
-    var text = "Hello, World!"
-}
